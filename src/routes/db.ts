a48a3b354e890be099d3d3b0733582a926e243a6
Mailson Albuquerque <62649846+mailsonsoc@users.noreply.github.com>
2022-01-24 10:36:18 -0300

    Update db.ts

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://atxfcweo:dgy0zuFMAvUcA9MJxBxhnQz745WCfCYL@kesavan.db.elephantsql.com/atxfcweo';
+const connectionString = '<sua conexão com o db>';
 
 const db = new Pool({connectionString});
 
